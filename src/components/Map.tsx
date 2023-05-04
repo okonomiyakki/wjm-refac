ccbd58ca1f290096f846b8efb8aad170da099e3c
okonomiyakki <parkji12532@naver.com>
2023-05-04 23:55:48 +0900

    feat : display kakao map

---
+++ src/components/Map.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+
+const kakao = (window as any).kakao;
+
+export default function Start() {
+    useEffect(() => {
+        const container = document.getElementById('map'),
+            mapOption = {
+                center: new kakao.maps.LatLng(37.566826004661, 126.978652258309),
+                level: 9
+            };
+        // 지도를 생성합니다    
+        const map = new kakao.maps.Map(container, mapOption);
+    }, []);
+
+    return (
+        <>
+            <div id="map" style={{ width: "100%", height: "800px" }}>
+            </div>
+        </>
+    )
+}
